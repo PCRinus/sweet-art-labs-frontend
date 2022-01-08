5a9bc491e3bc60cf2412a1aa875078db55d380a4
PCRinus <mircea.casapu@gmail.com>
2022-01-08 17:59:59 +0200

    feat: add sticky footer and placeholder components

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,34 @@
-<slot />
+<script>
+	import Footer from '../components/Footer.svelte';
+	import Navbar from '../components/Navbar.svelte';
+</script>
+
+<div class="content">
+	<Navbar />
+	<slot />
+</div>
+<div class="footer">
+	<Footer />
+</div>
 
 <style>
 	@tailwind base;
 	@tailwind components;
 	@tailwind utilities;
+
+	:global(html, body, #svelte) {
+		height: 100%;
+	}
+
+	:global(#svelte) {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.content {
+		flex: 1 0 auto;
+	}
+	.footer {
+		flex-shrink: 0;
+	}
 </style>
