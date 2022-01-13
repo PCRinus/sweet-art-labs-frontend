2e15922cb4c19915e6c9f11481b002df70082aab
PCRinus <mircea.casapu@gmail.com>
2022-01-13 21:56:23 +0200

    get context test

--- src/components/Footer/FooterContact.svelte
+++ src/components/Footer/FooterContact.svelte
@@ -2,14 +2,14 @@
 	import { getContext } from 'svelte';
 
 	const data = getContext('contactData');
-	const { address, hours, phone } = data.result.data.attributes;
-	const hoursArray = hours.split('\n');
+	// const { address, hours, phone } = data.result.data.attributes;
+	// const hoursArray = hours.split('\n');
 </script>
 
 <div class="contact my-4">
-	<h1>Adresa noastra: {address}</h1>
+	<!-- <h1>Adresa noastra: {address}</h1>
 	{#each hoursArray as hours}
 		<h1>{hours}</h1>
 	{/each}
-	<h1>Telefon: {phone}</h1>
+	<h1>Telefon: {phone}</h1> -->
 </div>
