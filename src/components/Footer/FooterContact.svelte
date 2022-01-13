38b80257cf7529e67ae428971ef01c899bbb4df9
PCRinus <mircea.casapu@gmail.com>
2022-01-13 22:02:16 +0200

    added onMount before data fetching from context

--- src/components/Footer/FooterContact.svelte
+++ src/components/Footer/FooterContact.svelte
@@ -1,15 +1,19 @@
 <script>
-	import { getContext } from 'svelte';
+	import { getContext, onMount } from 'svelte';
 
-	const data = getContext('contactData');
-	// const { address, hours, phone } = data.result.data.attributes;
-	// const hoursArray = hours.split('\n');
+  const hoursArray = [];
+
+	onMount(async () => {
+    const data = await getContext('contactData');
+    const { address, hours, phone } = data.result.data.attributes;
+    hoursArray = hours.split('\n');
+	});
 </script>
 
 <div class="contact my-4">
-	<!-- <h1>Adresa noastra: {address}</h1>
+	<!-- <h1>Adresa noastra: {address}</h1> -->
 	{#each hoursArray as hours}
 		<h1>{hours}</h1>
 	{/each}
-	<h1>Telefon: {phone}</h1> -->
+	<!-- <h1>Telefon: {phone}</h1> -->
 </div>
