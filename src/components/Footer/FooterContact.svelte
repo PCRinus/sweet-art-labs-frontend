65c8103ee656c47b79782a33f90f03810c7d964d
PCRinus <mircea.casapu@gmail.com>
2022-01-13 22:15:11 +0200

    process hour string

--- src/components/Footer/FooterContact.svelte
+++ src/components/Footer/FooterContact.svelte
@@ -1,18 +1,15 @@
 <script>
 	import { getContext, onMount } from 'svelte';
 
-	let hoursArray = [];
-
 	const data = getContext('contactData');
-	console.log(data);
 	const { address, phone, hours } = data.result.data.attributes;
-	console.log(address, phone, hours);
+	const hoursArray = hours.split('/n');
 </script>
 
 <div class="contact my-4">
-	<!-- <h1>Adresa noastra: {address}</h1> -->
+	<h1>Adresa noastra: {address}</h1>
 	{#each hoursArray as hours}
 		<h1>{hours}</h1>
 	{/each}
-	<!-- <h1>Telefon: {phone}</h1> -->
+	<h1>Telefon: {phone}</h1>
 </div>
