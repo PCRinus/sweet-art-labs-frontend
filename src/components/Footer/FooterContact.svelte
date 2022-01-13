866f3393bb482f7e5b1b9cc1663cb246e1093d31
PCRinus <mircea.casapu@gmail.com>
2022-01-13 19:17:47 +0200

    fix: more typos

--- src/components/Footer/FooterContact.svelte
+++ src/components/Footer/FooterContact.svelte
@@ -2,12 +2,12 @@
 	import { getContext } from 'svelte';
 
 	const data = getContext('contactData');
-	const { adress, hours, phone } = data.result.data.attributes;
+	const { address, hours, phone } = data.result.data.attributes;
 	const hoursArray = hours.split('\n');
 </script>
 
 <div class="contact my-4">
-	<h1>Adresa noastra: {adress}</h1>
+	<h1>Adresa noastra: {address}</h1>
 	{#each hoursArray as hours}
 		<h1>{hours}</h1>
 	{/each}
