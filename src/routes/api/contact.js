1348dbdfe7f9baf462e22f00bfa52900eba56877
PCRinus <mircea.casapu@gmail.com>
2022-01-13 19:06:23 +0200

    fix: api path typo

--- src/routes/api/contact.js
+++ src/routes/api/contact.js
@@ -1,7 +1,7 @@
 import { variables } from '$lib/variables';
 
 export const get = async () => {
-	const url = variables.apiPath + '/adress';
+	const url = variables.apiPath + '/address';
 	const data = await fetch(url);
 	const result = await data.json();
 
